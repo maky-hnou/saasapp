619f1ba0c21e2bd8f95dbfd75ea08cf66d7933f7
Hani YOUSFI <haniyousfi@gmail.com>
2017-10-11 16:00:07 +0000

    Added server side code for creating subscriptions for pro users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
+  end
 end
